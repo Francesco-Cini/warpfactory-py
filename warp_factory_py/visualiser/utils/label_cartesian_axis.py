cd1602b1da3f16cc680f8beb417a4c4f32b0bd3e
Francesco-Cini <cinifran010503@gmail.com>
2026-04-24 18:39:53 +0200

    v0.1.0
    - Alcubierre Metric Generation
    - Energy Tensor Calculation
    - Modified Plotter

--- warp_factory_py/visualiser/utils/label_cartesian_axis.py
+++
@@ -1,8 +0,0 @@
-def label_cartesian_axis(plane):
-    labels = ["t", "x", "y", "z"]
-    shown_planes = 1
-
-    x_label_name = labels(shown_planes[0])
-    y_label_name = labels(shown_planes[1])
-    
-    return x_label_name, y_label_name
