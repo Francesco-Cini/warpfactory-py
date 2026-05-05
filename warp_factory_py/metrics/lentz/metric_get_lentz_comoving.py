59ef233b12705a07d31c5d4468d0bbe92567c8c7
Francesco-Cini <cinifran010503@gmail.com>
2026-05-05 15:28:00 +0200

    Lentz Metric Finalisation

--- warp_factory_py/metrics/lentz/metric_get_lentz_comoving.py
+++ warp_factory_py/metrics/lentz/metric_get_lentz_comoving.py
@@ -5,7 +5,7 @@ from warp_factory_py.metrics.set_minkowski_three_plus_one import set_minkowski_t
 from warp_factory_py.units.universal_constants.c import c 
 from warp_factory_py.metrics.three_plus_one_builder import three_plus_one_builder
 
-def metric_get_lentz(grid_size, world_centre, v, scale, grid_scale):
+def metric_get_lentz_comoving(grid_size, world_centre, v, scale, grid_scale):
 
     if scale is None:
         scale = np.max(grid_size[1:4] / 7)
@@ -13,6 +13,9 @@ def metric_get_lentz(grid_size, world_centre, v, scale, grid_scale):
     if grid_scale is None:
         grid_scale = np.array([1, 1, 1, 1])
 
+    if grid_size[0] > 1:
+        raise Exception("The time grid is greater than 1, only a size of 1 can be used for Lentz")
+    
     metric = {}
 
     metric['params'] = {}
@@ -22,13 +25,15 @@ def metric_get_lentz(grid_size, world_centre, v, scale, grid_scale):
     metric['params']['velocity'] = v 
 
     metric['type'] = "metric"
-    metric['name'] = "Lentz"
-    metric['scaling'] = grid_scale 
+    metric['name'] = "Lentz Comoving"
+    metric['scaling'] = grid_scale
     metric['coords'] = "cartesian"
     metric['index'] = "covariant"
     metric['date'] = _date.today().isoformat()
 
     alpha, beta, gamma = set_minkowski_three_plus_one(grid_size)
+
+    t = 1 
 
     for i in range(grid_size[1]):
         for j in range(grid_size[2]):
@@ -36,58 +41,45 @@ def metric_get_lentz(grid_size, world_centre, v, scale, grid_scale):
 
                 x = i * grid_scale[1] - world_centre[1]
                 y = j * grid_scale[2] - world_centre[2]
-                
-                for t in range(grid_size[0]):
 
-                    xs = (t * grid_scale[0] - world_centre[0]) * v * c
+                WFX, WFY = get_warp_factor_by_region(x, y, scale)
 
-                    xp = x - xs 
-
-                    WFX, WFY = get_warp_factory_by_region(xp, y, scale)
-
-                    beta[0][t, i, j, k] = - WFX * v
-                    beta[1][t, i, j, k] = - WFY * v
-
+                beta[0][t, i, j, k] = v * (1 - WFX)
+                beta[1][t, i, j, k] = v * WFY
+    
     metric['tensor'] = three_plus_one_builder(alpha, beta, gamma)
 
-    return metric 
+    return metric
 
 def get_warp_factor_by_region(x_in, y_in, size_scale):
 
     x = x_in
     y = np.abs(y_in)
-
     WFX = 0
     WFY = 0
-    
-    if (x >= size_scale and x <= 2 * size_scale) and (x - size_scale >= y):
-        WFX = 0.2
-        WFY = 0
 
+    if (x >= size_scale and x <= 2 * size_scale) and (x - size_scale >= y):
+        WFX = -2
+        WFY = 0
     elif (x > size_scale and x <= 2 * size_scale) and (x - size_scale <= y) and (-y + 3 * size_scale >= x):
         WFX = -1
-        WFX = 1
-    
+        WFY =  1
     elif (x > 0 and x <= size_scale) and (x + size_scale > y) and (-y + size_scale < x):
         WFX = 0
-        WFY = 1
-
+        WFY =  1
     elif (x > 0 and x <= size_scale) and (x + size_scale <= y) and (-y + 3 * size_scale >= x):
         WFX = -0.5
-        WFY = 0.5
-
+        WFY =  0.5
     elif (x > -size_scale and x <= 0) and (-x + size_scale < y) and (-y + 3 * size_scale >= -x):
         WFX = 0.5
         WFY = 0.5
-
-    elif (x > -size_scale and x<= 0) and (x + size_scale <= y) and (-y + size_scale >= x):
+    elif (x > -size_scale and x <= 0) and (x + size_scale <= y) and (-y + size_scale >= x):
         WFX = 1
         WFY = 0
-    
     elif (x >= -size_scale and x <= size_scale) and (x + size_scale > y):
         WFX = 1
         WFY = 0
     
-    WFY = np.sign(y_in) * WFY
+    WFY = np.sign(y_in) * WFY 
 
     return WFX, WFY
